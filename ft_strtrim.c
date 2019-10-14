a224772e03769e1c0c0f6c582b1771dd996471cf
Ilyes DOUIDI <idouidi@e3r11p2.42.fr>
2019-10-14 20:46:42 +0200

    bosse bien

---
+++ ft_strtrim.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strtrim.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idouidi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/11 03:04:37 by idouidi           #+#    #+#             */
+/*   Updated: 2019/10/14 18:11:59 by idouidi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_count_set(char const *s1, char const *set)
+{
+	int	i;
+	int j;
+	int	stock;
+
+	i = 0;
+	j = 0;
+	stock = 0;
+	while (s1[i])
+	{
+		while (set[j] && set[j] != s1[i])
+			j++;
+		if (set[j] == '\0')
+			stock++;
+		j = 0;
+		i++;
+	}
+	return (stock);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		i;
+	char	*stock;
+	int		j;
+	int		k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	if (!(stock = (char *)malloc(sizeof(char) * ft_count_set(s1, set) + 1)))
+		return (NULL);
+	while (s1[i])
+	{
+		while (set[j] && set[j] != s1[i])
+			j++;
+		if (set[j] == '\0')
+		{
+			stock[k++] = s1[i];
+		}
+		j = 0;
+		i++;
+	}
+	stock[k] = '\0';
+	return (stock);
+}
