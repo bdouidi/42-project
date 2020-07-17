f65cea4f37783d32e915430f2b29a8719d264a9e
Othmane HABCHI <othabchi@e1r10p12.42.fr>
2020-07-17 03:09:59 +0200

    bordure fixe

--- cub3D/srcs/display/ft_raycasting.c
+++ cub3D/srcs/display/ft_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: othabchi <othabchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 03:12:26 by idouidi           #+#    #+#             */
-/*   Updated: 2020/07/16 23:46:25 by othabchi         ###   ########.fr       */
+/*   Updated: 2020/07/17 02:13:10 by othabchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,31 +24,34 @@ double	fisheye(t_data *d, double raydist)
 	return (raydist * cos(new_angle));
 }
 
-int		get_tex_color(t_data *d, int i)
+int		get_tex_color(t_data *d, int i, int line_height)
 {
 	char	*color;
 	double	tex_y;
 	double	tex_x;
 
-	tex_y = (d->y * 2 - d->res[1] + d->ray.line_height) *
-		(d->texture.height[i] / 2) / d->ray.line_height;
+	tex_y = (d->y * 2 - d->res[1] + line_height) *
+		(d->texture.height[i] / 2) / line_height;
 	if (i == 1 || i == 2)
 		tex_x = d->texture.width[i] - ((d->ray.x[0] - (int)d->ray.x[0]) *
 		d->texture.width[i]) - 1;
-	else if (i == 3 || i == 4)
+	else
 		tex_x = d->texture.width[i] - ((d->ray.y[1] - (int)d->ray.y[1]) *
 		d->texture.width[i]) - 1;
-	color = d->texture.addr[i] + (abs((int)tex_y) * d->texture.szl[i] + (int)tex_x * (d->texture.bpp[i] / 8));
+	color = d->texture.addr[i] + (abs((int)tex_y) * d->texture.szl[i] +
+	(int)tex_x * (d->texture.bpp[i] / 8));
 	return (*(unsigned int*)color);
 }
 
 void	draw_wall(t_data *d, double raydist, int i, int tmp_x)
 {
 	double	tmp_y;
+	int		true_line_height;
 
 	raydist = fisheye(d, raydist);
 	d->x = tmp_x;
 	d->ray.line_height = d->res[1] / raydist;
+	true_line_height = d->ray.line_height;
 	if (d->ray.line_height > d->res[1])
 		d->ray.line_height = d->res[1] - 1;
 	d->y = (d->res[1] / 2 - d->ray.line_height / 2) - 1;
@@ -59,7 +62,7 @@ void	draw_wall(t_data *d, double raydist, int i, int tmp_x)
 		i = ((d->player.pos_x - d->ray.x[1]) > 0) ? 3 : 4;
 	while (d->y <= d->ray.line_height + tmp_y)
 	{
-		my_mlx_pixel_put(d, get_tex_color(d, i));
+		my_mlx_pixel_put(d, get_tex_color(d, i, true_line_height));
 		d->y++;
 	}
 }
